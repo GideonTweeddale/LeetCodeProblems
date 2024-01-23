fd2e6676835fa285cd83cb3f49948771ad3d14cb
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-01-23 09:48:27 -0600

    Add BinarySearch

---
+++ LeetCodeProblems/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems;
+public class BinarySearch
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int num = nums[mid];
+
+            if (num == target)
+                return mid;
+
+            else if (num > target) // move left
+                right = mid - 1;
+
+            else // move right
+                left = mid + 1;
+        }
+
+        return -1;
+    }
+}
+
