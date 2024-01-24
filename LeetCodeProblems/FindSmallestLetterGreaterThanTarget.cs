1404263d83ff1217ae178c0d96553f999c2cda8c
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-01-24 10:50:56 -0600

    Add FindSmallestLetterGreaterThanTarget

---
+++ LeetCodeProblems/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace LeetCodeProblems;
+public class FindSmallestLetterGreaterThanTarget
+{
+
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        // iterating through the array should be O(n) time and O(1) space
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            if (letters[i] > target)
+            {
+                return letters[i];
+            }
+        }
+
+        return letters[0];
+    }
+
+    public char NextGreatestLetterBinary(char[] letters, char target)
+    {
+        // using binary search should be O(log N) time complexity
+        // in practice this is actually slower in the leetcode tests - it should be faster on larger datasets though 
+        int left = 0;
+        int right = letters.Length - 1;
+
+        if (letters[letters.Length - 1] <= target)
+            return letters[0];
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return letters[left];
+    }
+}
+
