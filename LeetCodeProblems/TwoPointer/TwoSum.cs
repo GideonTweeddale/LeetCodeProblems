6de2143c9ef1a7777a7aba301e93bf0c6b359794
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-02-02 12:36:23 -0600

    Add TwoSum

---
+++ LeetCodeProblems/TwoPointer/TwoSum.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.TwoPointer;
+public class TwoSumA
+{
+    public int[]? TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (dict.ContainsKey(complement))
+            {
+                return new int[] { dict[complement], i};
+            }
+
+            dict[nums[i]] = i;
+        }
+
+        return null;
+    }
+}
+
