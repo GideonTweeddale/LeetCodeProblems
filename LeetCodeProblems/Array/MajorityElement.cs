15f62abe31789bfe2ad95d33f7ad819ab0818662
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-02-06 10:39:26 -0600

    Add new solution for MajorityElement

--- LeetCodeProblems/Array/MajorityElement.cs
+++ LeetCodeProblems/Array/MajorityElement.cs
@@ -25,4 +25,28 @@ public static class MajorityElement5
 
         return largest;
     }
+    public static int MajorityElementB(int[] nums)
+    {
+        // this works because the element must appear more than half of the time
+        // therefore any other element will eventually have a count of zero and be replaced
+        // inevitably selecting the correct element
+        // if we get lucky and the correct element is selected early enough and appears more in the first half of the nums array
+        // we can return early
+        // this will run in O(N) time or better and O(1) space
+        int count = 0;
+        int? candidate = null;
+
+        foreach (int num in nums)
+        {
+            if (count == 0) 
+                candidate = num;
+
+            count += (num == candidate) ? 1 : -1;
+
+            if (count > nums.Length / 2) 
+                return candidate.Value;
+        }
+
+        return candidate.Value;
+    }
 }
