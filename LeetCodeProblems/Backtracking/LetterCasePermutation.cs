47511d141370f7b4573365d30754ac96428f68d9
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-02-23 10:30:39 -0600

    Add LetterCasePermutation

---
+++ LeetCodeProblems/Backtracking/LetterCasePermutation.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCodeProblems.Backtracking;
+public class LetterCasePermutation784
+{
+    public IList<string> LetterCasePermutation(string s)
+    {
+        // this is roughly O(N^2) time and space
+        // I wonder what the performance difference would be from using StringBuilder
+
+        IList<string> permuts = [];
+
+        PermuteLetterCases(s.ToCharArray(), permuts, 0);
+
+        return permuts;
+    }
+
+    private void PermuteLetterCases(char[] s, IList<string> permuts, int i)
+    {
+        // check for the base case
+        if (i == s.Length) 
+        {
+            permuts.Add(new string(s));
+            return;
+        } 
+
+        PermuteLetterCases(s, permuts, i + 1);
+
+        if (char.IsLetter(s[i]))
+        {
+            if (char.IsLower(s[i]))
+            {
+                s[i] = char.ToUpper(s[i]);
+            } else
+            {
+                s[i] = char.ToLower(s[i]);
+            }
+
+            PermuteLetterCases(s, permuts, i + 1);
+        }
+    }
+}
