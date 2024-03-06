40861a9e29763da70fd8976dc51c9aadb2f28d3a
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-03-06 17:34:06 -0600

    Add TargetSum

---
+++ LeetCodeProblems/Backtracking/TargetSum.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace LeetCodeProblems.Backtracking;
+public class TargetSum
+{
+    // backtracking solution
+    public int FindTargetSumWaysBacktracking(int[] nums, int target)
+    {
+        // like most of the backtracking solutions, this would be O(N^2) time and space roughly
+
+        int count = 0;
+
+        void BackTrack(int i, int sum)
+        {
+            if (i == nums.Length)
+            {
+                if (sum == target) count++;
+                return;
+            }
+
+            // add nums at i, increment i, and call backtrack
+            BackTrack(i + 1, sum + nums[i]);
+
+            // subtract nums at i, increment i, and call backtrack
+            BackTrack(i + 1, sum - nums[i]);
+        }
+
+        BackTrack(0, 0);
+
+        return count;
+    }
+
+    public int FindTargetSumWaysB(int[] nums, int target)
+    {
+        // this shoud be O(N * target) where n is number of elements in nums
+
+        Dictionary<(int, int), int> dp = new();
+
+        int Backtrack(int i, int current)
+        {
+            // the basecase
+            if (i >= nums.Length)
+            {
+                return target == current ? 1 : 0;
+            }
+
+            // if we have already computed the subtree, return that instead
+            if (dp.ContainsKey((current, i)))
+            {
+                return dp[(current, i)];
+            }
+
+            // call the backtrack function for the next index positive and negative
+            dp[(current, i)] = Backtrack(i + 1, current - nums[i]) +
+                                    Backtrack( i + 1, current + nums[i]);
+
+            return dp[(current, i)];
+        }
+
+        return Backtrack(0, 0);
+    }
+}
