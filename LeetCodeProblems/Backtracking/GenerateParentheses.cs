c1770fcb92ad13812ec38c700a6f22f40272a79f
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-03-06 10:28:45 -0600

    Add GenerateParentheses

---
+++ LeetCodeProblems/Backtracking/GenerateParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCodeProblems.Backtracking;
+public class GenerateParentheses22
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        IList<string> output = new List<string>();
+
+        void Backtrack(string str, int opened, int closed)
+        {
+            // we have reached the base case when we have added both the correct number of opening and closing parentheses
+            if (opened == n && closed == n)
+            {
+                output.Add(str);
+                return;
+            }
+
+            if (opened < n)
+            {
+                // add the opening parenthesis and increment the index
+
+                Backtrack(str + "(", opened + 1, closed);
+            }
+
+            if (closed < opened)
+            {
+                // add the closing parenthesis and call backtrack
+                Backtrack(str + ")", opened, closed + 1);
+            }
+        }
+
+        Backtrack("(", 1, 0);
+
+        return output;
+    }
+}
