4537a1f76eea628dff4e93fd0f1c217f1bbf9965
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-04-08 11:34:06 -0600

    Add new constructor to ListNode for dummy nodes

--- LeetCodeProblems/Utils/ListNode.cs
+++ LeetCodeProblems/Utils/ListNode.cs
@@ -3,10 +3,15 @@
 public class ListNode
 {
     public int val;
-    public ListNode next;
+    public ListNode? next;
     public ListNode(int x)
     {
         val = x;
         next = null;
+    }
+    public ListNode(int x, ListNode n)
+    {
+        val = x;
+        next = n;
     }
 }
