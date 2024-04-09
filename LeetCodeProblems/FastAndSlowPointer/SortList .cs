8f6bf72496739e4c4c0d7d0dccfa49f3266be3e3
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-04-09 16:45:19 -0600

    Add SortList

---
+++ LeetCodeProblems/FastAndSlowPointer/SortList .cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCodeProblems.FastAndSlowPointer;
+
+public class SortList148
+{
+    // intuition
+    // we can solve this with merge sort
+    // which would be O(n * logn) time complexity
+            
+    public ListNode SortList(ListNode head)
+    {
+        if (head == null || head.next == null) return head;
+
+        ListNode left = head;
+        ListNode mid = FindMiddle(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = SortList(left);
+        right = SortList(right);
+
+        return Merge(left, right);
+
+        ListNode FindMiddle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head.next;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+
+        ListNode Merge(ListNode left, ListNode right)
+        {
+            ListNode dummy = new ListNode(0);
+            ListNode tail = dummy;
+
+            while (left != null && right != null)
+            {
+                if (left.val < right.val)
+                {
+                    tail.next = left;
+                    left = left.next;
+                }
+                else
+                {
+                    tail.next = right;
+                    right = right.next;
+                }
+
+                tail = tail.next;
+            }
+
+            if (left != null) tail.next = left;
+
+            if (right != null) tail.next = right;
+
+            return dummy.next;
+        }
+    }
+}
+
