9b5d8a2ba53930b3a08cf28aab70f89ae7ed3c35
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-05-02 14:37:23 +0200

    Add LongestSubstringWithoutRepeatingCharacters

--- LeetCodeProblems/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeProblems/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.cs
@@ -2,81 +2,37 @@
 public class LongestSubstringWithoutRepeatingCharacters
 {
     // intuition
-    // this is effectively the two buckets sliding window problem, except that one of the buckets is size limited
-    // we move our right pointer through the list until the end
-    // for each iteration 
-    //      we add to the count in our hashtable for that character
-    //      we add to the total
-    //      while we have more than two items in our hashtable or neither item has a count less than k
-    //              move our left pointer, decrement the count/remove from the hashtable, and decrement the total
-    //      if total is greater than max set max to the total
+    // this is a sliding window problem where we need to keep track of which characters we've included in our sub array
+    // we move our right pointer, adding each character to a HashSet until we find a duplicate
+    // we move our left pointer, removing characters from the HashSet, until we no longer have a duplicate
+    // if our HashSet is longer than our previously observed longest HashSet, update max
     // return max
 
-    // this should be O(n) time complexity because we see each char in the string a maximum of twice O(2n) which simplyfies to O(n)
-    // this should be O(1) or constant space complexity because our hashtable will never have more than three values
+    // adding items to and looking up items in a unique HashSet is O(1) except in some very specific cases
+    // we will iterate through our array maximum twice, once for each of our pointers, which is O(n)
+    // this gives us a time complexity of O(n)
 
-    public int CharacterReplacement(string s, int k)
+    // our memory complexity will be also be constant O(1), given that there are a fixed number of English letters, digits, symbols, and spaces
+
+    public int LengthOfLongestSubstring(string s)
     {
-        Dictionary<char, int> counts = new(); // character, count
+        HashSet<char> chars = new();
 
-        int max = 0, maxCount = 0, left = 0, right = 0;
+        int max = 0, left = 0, right = 0;
 
-        while(right < s.Length)
+        while (right < s.Length)
         {
-            if (!counts.ContainsKey(s[right]))
+            if (!chars.Contains(s[right]))
             {
-                counts[s[right]] = 1;
+                chars.Add(s[right]);
+                right++;
+                max = Math.Max(max, chars.Count);
             }
             else
             {
-                counts[s[right]]++;
-            }
-
-            maxCount = Math.Max(maxCount, counts[s[right]]);
-
-            // the total number of elements less the larger element is the smaller element
-            // while the smaller element and therefore both elements is larger than k 
-            while (right - left + 1 - maxCount > k) 
-            {
-                counts[s[left]]--;
-
-                if (counts[s[left]] == 0) 
-                {
-                    counts.Remove(s[left]);
-                }
-
+                chars.Remove(s[left]);
                 left++;
             }
-
-            max = Math.Max(max, right - left + 1);
-
-            right++;
-        }
-
-        return max;
-    }
-
-    // try it with an array
-    // the array version is quicker
-    public int CharacterReplacementB(string s, int k)
-    {
-        int[] counts = new int[26]; // character, count // 26 because we are only using uppercase characters
-
-        int max = 0, maxCount = 0, left = 0, right = 0;
-
-        while(right < s.Length)
-        {
-            counts[s[right] - 'A']++;
-            maxCount = Math.Max(maxCount, counts[s[right] - 'A']);
-
-            while(right - left + 1 - maxCount > k)
-            {
-                counts[s[left] - 'A']--;
-                left++;
-            }
-
-            max = Math.Max(max, right - left + 1);
-            right++;
         }
 
         return max;
