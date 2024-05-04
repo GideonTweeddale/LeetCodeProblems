f695594b540db4529e56d87bd483d9a440e4d5af
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-05-04 06:33:36 +0200

    Add time and space complexity to KClosestPointsToOrigin

--- LeetCodeProblems/Heap/KClosestPointsToOrigin.cs
+++ LeetCodeProblems/Heap/KClosestPointsToOrigin.cs
@@ -9,6 +9,9 @@ public class KClosestPointsToOrigin
     // we can return the first k elements from the heap.
     // Once our heap is k elements, we could also remove the furthest element with each iteration, so that adding future elements is more efficient
 
+    // the time complexity is O(n log k) because we are iterating over n elements and adding to the heap is O(log k)
+    // the space complexity is O(k) because we are only storing k elements in the heap
+
     public int[][] KClosest(int[][] points, int k)
     {
         // sanity check
