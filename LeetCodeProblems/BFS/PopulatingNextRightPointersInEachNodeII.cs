73c753bd06d05c1cc1f540aa22bec6d14beac3bc
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-05-06 10:24:11 +0200

    Add PopulatingNextRightPointersInEachNodeII

--- LeetCodeProblems/BFS/PopulatingNextRightPointersInEachNodeII.cs
+++ LeetCodeProblems/BFS/PopulatingNextRightPointersInEachNodeII.cs
@@ -1,23 +1,23 @@
 ﻿namespace LeetCodeProblems.BFS;
-public class PopulationNextRightPointersInEachNode
+public class PopulatingNextRightPointersInEachNodeII
 {
     // intution
     // do BFS
     // on each level enqueue the next levels nodes
-    // and peek the next node in the queue and make it the next pointer for the current node
+    // peek the next node in the queue and if it is not null make it the next pointer for the current node
     // except for the last count where we make it null
 
     // this should be O(n) time and O(n) space because we will iterate through each node once
     // and our queue will have at most n/2 nodes in it because it will never have nodes from more than two levels at a time
 
-    public PerfectNode Connect(PerfectNode root)
+    public TreeNode Connect(TreeNode root)
     {
         if (root == null)
         {
             return null;
         }
 
-        Queue<PerfectNode> q = [];
+        Queue<TreeNode> q = [];
         q.Enqueue(root);
 
         while(q.Any())
@@ -26,11 +26,15 @@ public class PopulationNextRightPointersInEachNode
 
             for (int i = 0; i < count; i++)
             {
-                PerfectNode node = q.Dequeue();
+                TreeNode node = q.Dequeue();
 
-                if (i < count - 1)
+                if (i < count - 1 && q.Peek() != null)
                 {
                     node.next = q.Peek();
+                }
+                else
+                {
+                    node.next = null; // this probably isn't necessary, but it doesn't cost much to be explicit here
                 }
 
                 if (node.left != null)
