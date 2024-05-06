77fc27f4588642230fcff6c155bfdc3ca2e39a79
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-05-06 08:13:16 +0200

    Fix BinaryTreeLevelOrderReversalII name

---
+++ LeetCodeProblems/BFS/BinaryTreeLevelOrderReversalII.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCodeProblems.BFS;
+public class BinaryTreeLevelOrderReversalII
+{
+    // intuition
+    // we can solve this problem using BFS.
+    // create a list of lists of nodes to store the output
+    // Starting with the root node,
+    // add the node to the output list at the index matching the depth
+    // recursively call BFS on each child node, until we reach a leaf node
+    // with each call increment and pass the depth
+    // reverse the ouptut list
+    // return the output
+
+    // we should only see each node once during BFS and once during reversal
+    // so this will complete in O(n) time and we will have a function call in the stack for each node so our memory will be O(n) as well
+    public IList<IList<int>> LevelOrderBottom(TreeNode root)
+    {
+        List<IList<int>> output  = new List<IList<int>>();
+
+        output = BFS(root, 0);
+        output.Reverse();
+
+        return output;
+
+        // helper BFS function
+        List<IList<int>> BFS(TreeNode node, int depth)
+        {
+            if (node == null)
+            {
+                return output;
+            }
+
+            if (output.Count == depth)
+            {
+                output.Add([]);
+            }
+
+            BFS(node.left, depth + 1);
+            BFS(node.right, depth + 1);
+
+            output[depth].Add(node.val);
+
+            return output;
+        }
+    }
+}
+
