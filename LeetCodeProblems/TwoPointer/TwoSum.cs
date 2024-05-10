7f990d0d3186947931e6d5c4502d3056c5dc6338
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-05-10 11:33:27 +0200

    Add intuition and Big O analysis to TwoSum

--- LeetCodeProblems/TwoPointer/TwoSum.cs
+++ LeetCodeProblems/TwoPointer/TwoSum.cs
@@ -1,6 +1,15 @@
 ﻿namespace LeetCodeProblems.TwoPointer;
-public class TwoSumA
+public class TwoSum1
 {
+    // intuition
+    // iterate through our nums
+    //      generate the complement by subracting the num from the target
+    //      if our hashmap contains the complement, it means that our num and the complement both exists: we have found the answer and can return the indicies
+    //      otherwise add the num to the hashmap in the hopes that it will be the complement for a future num
+
+    // this implementation is O(n) time and space where n is the length of nums
+    // O(n) time because we iterate through nums only once
+    // and O(n) space becuase, in the case where our complement includes the last num in nums, we would add n-1 or n items to our hashmap
     public int[]? TwoSum(int[] nums, int target)
     {
         Dictionary<int, int> dict = new();
