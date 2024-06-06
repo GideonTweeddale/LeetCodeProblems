d8d520a8b79221461d1cf2cc61a2705a56f5d3d9
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-06 10:57:32 +0200

    Add FindTheIndexOfTheFirstOccurenceInAString

--- LeetCodeProblems/ArraysAndStrings/FindTheIndexOfTheFirstOccurenceInAString.cs
+++ LeetCodeProblems/ArraysAndStrings/FindTheIndexOfTheFirstOccurenceInAString.cs
@@ -9,26 +9,17 @@ public class FindTheIndexOfTheFirstOccurenceInAString
     // in practice, this solution should be fine because the strings are unlikely to be very long 
     // and the first character of needle is unlikely to appear that often in haystack
 
-    // however, 
-
-    // notable edge cases
-    // where needle is longer than haystack and therefore cannot be found within it
-
-    // naive solution
+    // naive solution - which is > 90% in both speed and memory on leetcode
     public int StrStr(string haystack, string needle)
     {
-        if (haystack.Length == 0 || needle.Length == 0 || haystack.Length < needle.Length) return -1;
-
-        for (int i = 0; i < haystack.Length; i++)
+        for (int i = 0; i < haystack.Length + 1 - needle.Length; i++)
         {
             for (int j = 0; j < needle.Length; j++)
             {
-                if (i + j >= haystack.Length || haystack[i + j] != needle[j]) break;
+                if (haystack[i + j] != needle[j]) break;
 
                 if (j == needle.Length -1) return i;
             }
-
-            if (i + needle.Length > haystack.Length) break;
         }
 
         return -1;
