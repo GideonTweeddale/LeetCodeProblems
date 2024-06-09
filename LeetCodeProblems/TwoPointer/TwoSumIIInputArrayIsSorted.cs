e20641c6f74a7b56ea2d85d6feffb70308ad8265
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-09 18:28:08 +0200

    Add TwoSumIIInputArrayIsSorted

---
+++ LeetCodeProblems/TwoPointer/TwoSumIIInputArrayIsSorted.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeProblems.TwoPointer;
+public class TwoSumIIInputArrayIsSorted
+{
+    // intuition
+    // because the array is sorted in ascending order
+    // we can use the two pointer approach
+    // starting with the outer indexes, if the sum of the values is greater than the target we need to move the right pointer inwards
+    // if the sum is smaller then we need to move the left pointer inwards
+
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            // base case
+            if (numbers[left] + numbers[right] == target)
+            {
+                return [left+1, right+1]; // add one becuase the return array is supposed to be 1-indexed
+            }
+
+            if(numbers[left] + numbers[right] > target)
+                right--;
+            else 
+                left++;
+        }
+
+        // we should never reach this - ideally the method definition should be changed to reflect the fact that we are guaranteed to have a valid sum
+        // at the very least we should be throwing an error 
+        return null; 
+    }
+}
