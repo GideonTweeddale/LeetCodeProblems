071f0190568b82e0d30930eb327611bc1f4bd24e
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-11 14:28:18 +0200

    Add Intuition and time and space complexity analysis to LRUCache

--- LeetCodeProblems/LinkedList/LRUCache.cs
+++ LeetCodeProblems/LinkedList/LRUCache.cs
@@ -19,7 +19,26 @@ public class Node
 public class LRUCache
 {
     // intuition
+    // we can use a doubly linked list to hold our queue of most recently seen items
+    // and a map to hold references to the nodes in our double linked list accessible by key
+    // we maintain a pointer to the first and the last item in the doubly linked list
 
+    // we add items by creating a node holding the key and value
+    // then adding it to our double linked list by inserting them before the last item pointer
+    // and then adding the node to our map with the key 
+
+    // we remove items by assigning the pointers of the previous and next nodes in the double linked list to each other
+    // and then removing the key-node pair from the map
+
+    // when put is called, we remove our item if it exists, and then re-add it, therby insuring that it is the most recent
+
+    // when get is called, we do the same, except that we return -1 if it is not in our map
+
+    // the map holding the nodes means that we can find any node in O(1) time
+    // and the nodes holding references to the next and previous node means that we can do our adds and deletes in O(1) time
+    // therefore our put and get are both O(1)
+    // the overall space complexity of our entire LRUCache is O(2n) or just O(n) in the worst case where n is the capacity
+    // and n or more nodes were added to the cache
 
     private Node oldest;
     private Node newest;
