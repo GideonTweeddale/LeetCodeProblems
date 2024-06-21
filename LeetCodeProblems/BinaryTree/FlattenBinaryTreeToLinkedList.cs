dce78707519a5f9b67923cf6902efcd8f04ccc42
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-21 11:19:25 +0200

    Add new FlattenBinaryTreeToLinkedList solution

--- LeetCodeProblems/BinaryTree/FlattenBinaryTreeToLinkedList.cs
+++ LeetCodeProblems/BinaryTree/FlattenBinaryTreeToLinkedList.cs
@@ -6,6 +6,7 @@ public class FlattenBinaryTreeToLinkedList
     // we can do this in O(n) time, visiting each node once
     // I don't think it is possible to do it faster because we need to change the pointers on every node
     // and we need to visit the last node to know that it is the last node
+    // and the memory will be O(h) where h is the height of the tree
 
     // the trick is to recurse right first and find the final node
     // and the build the linkedlist up in reverse
@@ -32,6 +33,28 @@ public class FlattenBinaryTreeToLinkedList
             node.right = node.left;
             node.left = null;    
         }
+    }
+
+    // to do this more intuitively - at least for me
+    // we can go left to right
+    public void FlattenB(TreeNode root) => DFS(root);
+    private TreeNode DFS(TreeNode node)
+    {        
+        if (node == null) return null;
+
+        TreeNode leftTail = DFS(node.left);
+        TreeNode rightTail = DFS(node.right);
+
+        if (node.left != null)
+        {
+            leftTail.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
+
+        if (rightTail != null) return rightTail;
+        if (leftTail != null) return leftTail;
+        return node;
     }
 }
 
