bb6f6b497358aa9330b7337a5f4e1e9be5386d59
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-26 11:07:57 +0200

    Add Trie using map

---
+++ LeetCodeProblems/Trie/TrieWithMap.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodeProblems.Trie;
+
+public class TrieMap
+{
+    private Dictionary<char, TrieMap> trieMap;
+    
+    public TrieMap()
+    {
+        trieMap = new Dictionary<char, TrieMap>();
+    }
+
+    public void Insert(string word) 
+    {
+        Dictionary<char, TrieMap> trieNode = trieMap;
+
+        foreach (char c in word)
+        {
+            if (!trieNode.ContainsKey(c))
+            {
+                trieNode[c] = new TrieMap();
+            }
+            
+            trieNode = trieNode[c].trieMap;
+        }
+
+        trieNode['.'] = new TrieMap();
+    }
+
+    public bool Search(string word) 
+    {
+        Dictionary<char, TrieMap> trieNode = trieMap;
+
+        foreach (char c in word)
+        {
+            if (!trieNode.ContainsKey(c))
+            {
+                return false;
+            }
+
+            trieNode = trieNode[c].trieMap;
+        }
+
+        return trieNode.ContainsKey('.');
+    }
+
+    public bool StartsWith(string prefix) 
+    {
+        Dictionary<char, TrieMap> trieNode = trieMap;
+
+        foreach (char c in prefix)
+        {
+            if (!trieNode.ContainsKey(c))
+            {
+                return false;
+            }
+
+            trieNode = trieNode[c].trieMap;
+        }
+
+        return true;
+    }
+}
