108968ef44b3d17cad3775745f4ef83c6f00c569
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2024-06-27 11:36:19 +0200

    Add DesignAddAndSearchWordsDataStructure

---
+++ LeetCodeProblems/Trie/DesignAddAndSearchWordsDataStructure.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCodeProblems.Trie;
+
+public class WordDictionary {
+    // we can solve this using a trie
+    // I like to implement trie's as a cascading set of maps
+
+    // the difference from a classic trie, is that once we have a wildcard, we'll have to bruteforce search for any children that match
+    Trie trie;
+
+    public WordDictionary() {
+        trie = new();
+    }
+    
+    public void AddWord(string word) {
+        Trie node = trie;
+
+        foreach (char c in word)
+        {
+            if (!node.map.ContainsKey(c))
+            {
+                node.map[c] = new Trie();
+            }
+
+            node = node.map[c];
+        }
+
+        node.map['~'] = new Trie();
+    }
+    
+    public bool Search(string word) {
+        return Search(word, trie);
+    }
+
+    private bool Search(string chars, Trie node)
+    {
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char c = chars[i];
+
+            if (c == '.')
+            {
+                foreach(Trie letter in node.map.Values)
+                {
+                    if (Search(chars[(i+1)..], letter))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (!node.map.ContainsKey(c))
+            {
+                return false;
+            }
+
+            node = node.map[c];
+        }
+
+        return node.map.ContainsKey('~');
+    }
+
+    class Trie {
+        public Dictionary<char, Trie> map = new();
+    }
+}
